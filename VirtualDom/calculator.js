e16404598fabd208cd50a965a2bbe37d07d156d4
Arpit Kumar <arpitkumar19842@gmail.com>
2025-10-14 16:06:56 +0530

    Started Learning React

---
+++ VirtualDom/calculator.js
@@ -0,0 +1,3 @@
+import add, { multiply } from './math.js';
+console.log(add(8, 9));
+console.log(multiply(3, 4));
---
+++ VirtualDom/math.js
@@ -0,0 +1,6 @@
+export default function add(a, b){
+    return a + b;
+}
+export function multiply(a, b) {
+    return a * b;
+}
