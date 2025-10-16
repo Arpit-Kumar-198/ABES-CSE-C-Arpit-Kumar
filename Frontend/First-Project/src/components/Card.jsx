d8d644ac5960aa3605ae47dfe4e9dd43a486b9fc
Arpit Kumar <arpitkumar19842@gmail.com>
2025-10-16 10:28:30 +0530

    Learned components in React

---
+++ Frontend/First-Project/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import myImg from '../assets/download.jpeg';
+
+
+function Card() {
+  return (
+    <>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={myImg} className="card-img-top" alt="myimg" />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card’s content.
+          </p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
